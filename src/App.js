b6314451eadeb9955559670d18cc50d65d5200f3
Cian Martyn <G00404885@atu.ie>
2024-08-08 15:19:49 +0100

    Add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Content from './components/content';
+import Footer from './components/footer';
+import Header from './components/header';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Create from './components/create';
+import Publish from './components/publish';
+import Edit from './components/edit';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Music</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/create">Publish</Nav.Link>
+            <Nav.Link href="/publish">Discover</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<Content></Content>}></Route>
+        <Route path='/publish' element={<Publish></Publish>}></Route>
+        <Route path='/create' element={<Create></Create>}></Route>
+        <Route path='/edit/:id' element={<Edit></Edit>}></Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
